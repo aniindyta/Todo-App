005b61fc0283789cdf3e32d231e40332169d80f3
aniindyta <aninditaputri055@gmail.com>
2023-12-01 06:08:34 +0700

    show notification with pending intent

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,20 +1,28 @@
 package com.dicoding.todoapp.notification
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
+import com.dicoding.todoapp.utils.DateConverter
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
 import com.dicoding.todoapp.utils.TASK_ID
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
     private val channelName = inputData.getString(NOTIFICATION_CHANNEL_ID)
+    private val taskRepository = TaskRepository.getInstance(ctx)
 
     private fun getPendingIntent(task: Task): PendingIntent? {
         val intent = Intent(applicationContext, DetailTaskActivity::class.java).apply {
@@ -30,8 +38,41 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         }
     }
 
+    @SuppressLint("NotificationPermission")
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        val nearestActiveTask = taskRepository.getNearestActiveTask()
+        val pendingIntent = getPendingIntent(nearestActiveTask)
+
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
+            setSmallIcon(R.drawable.ic_notifications)
+            setContentIntent(pendingIntent)
+            setContentTitle(nearestActiveTask.title)
+            setContentText(
+                applicationContext.resources.getString(
+                    R.string.notify_content,
+                    DateConverter.convertMillisToString(nearestActiveTask.dueDateMillis)
+                )
+            )
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = channelName
+            val notificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            notificationBuilder.setChannelId(NOTIFICATION_CHANNEL_ID)
+        }
+
+        val notification = notificationBuilder.build()
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(nearestActiveTask.id, notification)
         return Result.success()
     }
 
