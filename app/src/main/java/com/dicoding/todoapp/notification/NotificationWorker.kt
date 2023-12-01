0605dfb3bab1336f3c83c9622a6b021751baf358
aniindyta <aninditaputri055@gmail.com>
2023-12-01 10:27:02 +0700

    show notification

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.work.Worker
@@ -42,7 +43,9 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
         val nearestActiveTask = taskRepository.getNearestActiveTask()
+
         val pendingIntent = getPendingIntent(nearestActiveTask)
+        Log.d("NotificationWorker", "Nearest task title: ${nearestActiveTask.title}")
 
         val notificationBuilder = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
             setSmallIcon(R.drawable.ic_notifications)
@@ -73,6 +76,7 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val notificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(nearestActiveTask.id, notification)
+
         return Result.success()
     }
 
